faf0eb2417ec03a45b748f4e942e42c802d98818
Esp-32-button <unknownentity107@gmail.com>
2025-01-27 00:08:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Store the current servo state
 let servoState = "OFF";
 
